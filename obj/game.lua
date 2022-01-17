50daf6cd277f491cdb77d114c526bbb65b6ce7e2
christianwhejlesen <wiehlhejlesen@outlook.dk>
2022-01-17 20:36:25 +0100

    added default value to bgColor

--- obj/game.lua
+++ obj/game.lua
@@ -1,14 +1,14 @@
 --GAME OBJECT
 
 Game = {}
-    local bgColor;
+    local bgColor = { 36/255, 92/255, 117/255, 100/100 };
 
     function Game.getbgColor()
         return bgColor;
     end
 
     function Game.init( in_bgColor, in_lineColor)
-        bgColor = in_bgColor or { 36/255, 92/255, 117/255, 100/100 };
+        bgColor = in_bgColor or bgColor;
     end
 
 return Game;
